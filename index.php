29ff4868b77b4781c4f4e407c4e03c68e04d88db
Lucas Arbex <lucasarbexmb@gmail.com>
2019-01-19 14:19:36 -0200

    Added categories

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use \Slim\Slim;
 use \Projeto\Page;
 use \Projeto\PageAdmin;
 use \Projeto\Model\User;
+use \Projeto\Model\Category;
 
 $app = new \Slim\Slim();
 
@@ -218,6 +219,93 @@ $app->post("/admin/forgot/reset", function(){
 	]);
 
 	$page->setTpl('forgot-reset-success');
+
+});
+
+$app->get("/admin/categories", function(){
+
+	User::verifyLogin();
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl('categories', [
+		"categories" => $categories
+	]);
+
+});
+
+$app->get("/admin/categories/create", function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+
+});
+
+$app->post("/admin/categories/create", function(){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+$app->get("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update", [
+		"category" => $category->getValues()
+	]);
+
+});
+
+$app->post("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
 
 });
 
---
+++ vendor/projeto/php-classes/src/Model/Category.php
@@ -0,0 +1,49 @@
+<?php 
+
+namespace Projeto\Model;
+
+use \Projeto\DB\Sql;
+use \Projeto\Model;
+use \Projeto\Mailer;
+
+class Category extends Model {
+
+	public static function listAll()
+	{
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_categories ORDER BY descategory");
+	}
+
+
+	public function save()
+	{
+		$sql = new Sql();
+		$results = $sql->select("CALL sp_categories_save(:idcategory, :descategory)", array(
+			":idcategory"=>$this->getidcategory(),
+			":descategory"=>$this->getdescategory()
+		));
+		$this->setData($results[0]);
+	}
+
+
+	public function get($idcategory)
+	{
+		$sql = new Sql();
+		$results = $sql->select("SELECT * FROM tb_categories WHERE idcategory = :idcategory", [
+			"idcategory" => $idcategory
+		]);
+		$this->setData($results[0]);
+	}
+
+
+	public function delete()
+	{
+		$sql = new Sql();
+		$sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory", [
+			"idcategory" => $this->getidcategory()
+		]);
+	}
+
+}
+
+ ?>
